b8257df4d16edd11b4477e3ca3dce1d73a00dae8
Christopher.D.Oliver@gmail.com <Christopher.D.Oliver@gmail.com@d197f762-2fa0-c7fc-eed2-f533f656fa44>
2013-08-20 00:17:23 +0000

    inching

--- trunk/demo.sh
+++ trunk/demo.sh
@@ -4,4 +4,4 @@ export LIB=./lib
 export DYLD_LIBRARY_PATH=$LIB/macosxuniversal
 #JAVA=/Library/Java/JavaVirtualMachines/jdk1.7.0_17.jdk/Contents/Home/bin/java
 JAVA=java
-$JAVA -Dapple.awt.graphics.UseQuartz=true -Df3.debug.anim=false -d32 -Xmx1G -Xbootclasspath/p:lib/jsr166.jar -classpath ${LIB}/f3rt.jar:${LIB}/jogl.all.jar:${LIB}/gluegen-rt.jar:${LIB}/jogl.cg.jar:${LIB}/f3.svg.awt.jar:${LIB}/f3.awesomium.jogl.jar:${LIB}/f3.cg.jogl.awt.jar:build/classes:${LIB}/json-simple-1.1.1.jar org.f3.runtime.Main expr.Main $*
+$JAVA -Dapple.awt.graphics.UseQuartz=true -Df3.jogl.stage.enable.textured.text=false -Df3.debug.anim=false -d32 -Xmx1G -Xbootclasspath/p:lib/jsr166.jar -classpath ${LIB}/f3rt.jar:${LIB}/jogl.all.jar:${LIB}/gluegen-rt.jar:${LIB}/jogl.cg.jar:${LIB}/f3.svg.awt.jar:${LIB}/f3.awesomium.jogl.jar:${LIB}/f3.cg.jogl.awt.jar:build/classes:${LIB}/json-simple-1.1.1.jar org.f3.runtime.Main expr.Main $*
